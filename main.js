b01ab0c37007a6b5036cc428f1ac3f96c71ebc9a
Nicy James <95037452+NicyJames@users.noreply.github.com>
2024-09-28 21:45:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+// Show a notification when the 'Order Now' button is clicked
+document.querySelector('.btn').addEventListener('click', function() {
+    alert('Thank you for your order! You will be redirected shortly.');
+});
